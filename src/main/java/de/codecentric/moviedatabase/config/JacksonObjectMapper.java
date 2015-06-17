6046da609e6b547adfd6a51161ac76a0748a1884
Reto Kleeb <reto.kleeb@elca.ch>
2015-06-17 08:59:04 +0200

    migrated to java8, recent spring version

--- src/main/java/de/codecentric/moviedatabase/config/JacksonObjectMapper.java
+++ src/main/java/de/codecentric/moviedatabase/config/JacksonObjectMapper.java
@@ -1,18 +1,17 @@
 package de.codecentric.moviedatabase.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.annotation.PostConstruct;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.SerializationConfig;
-import org.codehaus.jackson.map.DeserializationConfig;
 
 public class JacksonObjectMapper extends ObjectMapper {
-	
-	
-	@PostConstruct
-	public void afterPropertiesSet() throws Exception {
-		configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
-	}
-	
+
+
+    @PostConstruct
+    public void afterPropertiesSet() throws Exception {
+        //configure(DeserializationConfig. Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        //configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
 }
